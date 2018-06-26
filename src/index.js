1874ee34275aa51c5e4c1139bf90b069ae75d21f
Simon Willison <simonw@eventbrite.com>
2018-06-25 20:05:14 -0700

    Scan for .export-link a[href=*json] and inject necessary markup

--- src/index.js
+++ src/index.js
@@ -3,20 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import DatasetteVega from './DatasetteVega';
 
-ReactDOM.render(
-  /*<App columns={[
-    'airline',
-    'avail_seat_km_per_week',
-    'incidents_85_99',
-    'fatal_accidents_85_99',
-    'fatalities_85_99',
-    'incidents_00_1',
-    'fatal_accidents_00_14',
-    'fatalities_00_14'
-  ]}
-  json_url="https://fivethirtyeight.datasettes.com/fivethirtyeight-45d758d/bechdel%2Fmovies.json?_shape=array"
-  // json_url="https://fivethirtyeight.datasettes.com/fivethirtyeight-45d758d/airline-safety%2Fairline-safety.json?_shape=array"
-  */
-  <DatasetteVega base_url="https://fivethirtyeight.datasettes.com/fivethirtyeight-45d758d.json?sql=select+rowid%2C+*+from+%5Bbechdel%2Fmovies%5D+order+by+random%28%29+limit+200"
-  />, document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  const jsonEl = document.querySelector('.export-links a[href*=json]');
+  if (jsonEl) {
+    // Create elements to inject this into
+    let visWrapper = document.createElement('div');
+    visWrapper.style.overflow = 'hidden';
+    let vis = document.createElement('div');
+    let visTool = document.createElement('div');
+    vis.setAttribute('id', 'vis');
+    visWrapper.appendChild(vis);
+    let table = document.querySelector('table.rows-and-columns');
+    table.parentNode.insertBefore(visWrapper, table);
+    table.parentNode.insertBefore(visTool, visWrapper);
+    let jsonUrl = jsonEl.getAttribute('href');
+    // Add _shape=array
+    jsonUrl += (jsonUrl.indexOf('?') > -1) ? '&' : '?';
+    jsonUrl += '_shape=array'
+    ReactDOM.render(
+      <DatasetteVega base_url={jsonUrl} />, visTool
+    );
+  }
+});
