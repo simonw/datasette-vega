6fe66c4c9d0dba0a6eb636583914d2ed7d3d72a9
Simon Willison <swillison@gmail.com>
2025-11-02 10:26:58 -0800

    Fix for httpx.AsyncClient(app=app)
    Refs https://github.com/simonw/datasette/issues/2549

--- tests/test_vega.py
+++ tests/test_vega.py
@@ -1,13 +1,11 @@
-from datasette.app import Datasette
 import pytest
-import httpx
+from datasette.app import Datasette
 
 
 @pytest.mark.asyncio
 async def test_plugin_is_installed():
-    app = Datasette([], memory=True).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/plugins.json")
-        assert response.status_code == 200
-        installed_plugins = {p["name"] for p in response.json()}
-        assert "datasette-vega" in installed_plugins
+    ds = Datasette([], memory=True)
+    response = await ds.client.get("/-/plugins.json")
+    assert response.status_code == 200
+    installed_plugins = {p["name"] for p in response.json()}
+    assert "datasette-vega" in installed_plugins
